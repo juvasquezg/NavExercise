c1ababb45c874fde1c0a598b1150aa1a52169673
julianvasquez <julian.vasquez@netuxtecnologia.com>
2015-04-05 22:04:21 -0500

    update Huge app, review

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,52 +4,53 @@
 var gulp = require('gulp'),
 		connect   = require('gulp-connect'),
 		stylus    = require('gulp-stylus'),
-    nib       = require('nib'),
-    jshint    = require('gulp-jshint'),
+		nib       = require('nib'),
+		jshint    = require('gulp-jshint'),
+		stylish   = require('jshint-stylish'),
 		historyApiFallback = require('connect-history-api-fallback');
 
 
 // Web server development
 gulp.task('server', function() {
-  connect.server({
-    root: './public',
-    hostname: '0.0.0.0',
-    port: 8080,
-    livereload: true,
-    middleware: function(connect, opt) {
-      return [ historyApiFallback ];
-    }
-  });
+	connect.server({
+		root: './public',
+		hostname: '0.0.0.0',
+		port: 8080,
+		livereload: true,
+		middleware: function(connect, opt) {
+			return [ historyApiFallback ];
+		}
+	});
 });
 
 // Search javascripts errors
 gulp.task('jshint', function() {
-  return gulp.src('./public/scripts/**/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'));
+	return gulp.src('./public/scripts/**/*.js')
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(jshint.reporter('fail'));
 });
 
 // Compile Stylus to css
 gulp.task('css', function() {
-  gulp.src('./public/styles/main.styl')
-    .pipe(stylus({ use: nib() }))
-    .pipe(gulp.dest('./public/styles'))
-    .pipe(connect.reload());
+	gulp.src('./public/styles/main.styl')
+		.pipe(stylus({ use: nib() }))
+		.pipe(gulp.dest('./public/styles'))
+		.pipe(connect.reload());
 });
 
 // reload browser if HTML changed
 gulp.task('html', function() {
-  gulp.src('./public/**/*.html')
-    .pipe(connect.reload());
+	gulp.src('./public/**/*.html')
+		.pipe(connect.reload());
 });
 
 
 // watch
 gulp.task('watch', function() {
-  gulp.watch(['./public/**/*.html'], ['html']);
-  gulp.watch(['./public/styles/**/*.styl'], ['css']);
-  gulp.watch(['./public/scripts/**/*.js', './Gulpfile.js'], ['jshint']);
+	gulp.watch(['./public/**/*.html'], ['html']);
+	gulp.watch(['./public/styles/**/*.styl'], ['css']);
+	gulp.watch(['./public/scripts/**/*.js', './Gulpfile.js'], ['jshint']);
 });
 
 gulp.task('default', ['server', 'watch']);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,40 +6,40 @@
 
 
 (function() {
-	// Return ElementById
-	var $ = function (id) {
-	 return document.getElementById(id);
-	};
+  // Return ElementById
+  var $ = function (id) {
+    return document.getElementById(id);
+  };
 
-	var _$ = function (c) {
-		return document.getElementsByClassName(c);
-	};
+  var _$ = function (c) {
+    return document.getElementsByClassName(c);
+  };
 
-	var Navegation = function (id, label, url, items) {
-		this.id = id;
-		this.label = label;
-		this.url = url;
-		this.items = items;
-	};
+  var Navegation = function (id, label, url, items) {
+    this.id = id;
+    this.label = label;
+    this.url = url;
+    this.items = items;
+  };
 
   // globals
   var navegations = [];
 
 
-	function doRequest (callback) {
-		var xhr = new XMLHttpRequest();
-		xhr.overrideMimeType('application/json');
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState === 4 && xhr.status === 200) {
-				var textResponse = xhr.responseText;
-				var jsonResponse = JSON.parse(textResponse);
-				var items = jsonResponse.items;
-				callback(null, items);
-			}
-		};
-		xhr.open('GET', '/api/nav.json', true);
-		xhr.send();
-	}
+  function doRequest (callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.overrideMimeType('application/json');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var textResponse = xhr.responseText;
+        var jsonResponse = JSON.parse(textResponse);
+        var items = jsonResponse.items;
+        callback(null, items);
+      }
+    };
+    xhr.open('GET', '/api/nav.json', true);
+    xhr.send();
+  }
 
 
   function init() {
@@ -57,107 +57,138 @@
 
   function createNav(navegations) {
       // Create Navegation
-		for (var i = 0; i < navegations.length; i++) {
-    	$('items').appendChild(buildItem(navegations[i]));
-    	if (navegations[i].items.length !== 0) {
-    		$(navegations[i].id.toString()).appendChild(createUl(navegations[i].id));
-    		for (var j = 0; j < navegations[i].items.length; j++) {
-    			$('subitems' + navegations[i].id.toString()).appendChild(buildSubItem(navegations[i].items[j]));
-    		}
-    	}
+    for (var i = 0; i < navegations.length; i++) {
+      $('items').appendChild(buildItem(navegations[i]));
+      if (navegations[i].items.length !== 0) {
+        $(navegations[i].id.toString()).appendChild(createUl(navegations[i].id));
+        for (var j = 0; j < navegations[i].items.length; j++) {
+          $('subitems' + navegations[i].id.toString()).appendChild(buildSubItem(navegations[i].items[j]));
+        }
+      }
     }
   }
 
 
   function createUl(id) {
-		var ul = document.createElement('ul');
-		ul.setAttribute('id', 'subitems' + id.toString());
-		ul.setAttribute('class', 'subitems');
-		return ul;
-	}
+    var ul = document.createElement('ul');
+    ul.setAttribute('id', 'subitems' + id.toString());
+    ul.setAttribute('class', 'subitems');
+    return ul;
+  }
 
 
-	function buildSubItem(request) {
-		var innerLink = document.createElement('a');
-		innerLink.setAttribute('href', request.url);
-		innerLink.appendChild(document.createTextNode(request.label));
-		var li = document.createElement('li');
-		li.appendChild(innerLink);
-		return li;
-	}
+  function buildSubItem(request) {
+    var innerLink = document.createElement('a');
+    innerLink.setAttribute('href', request.url);
+    innerLink.appendChild(document.createTextNode(request.label));
+    var li = document.createElement('li');
+    li.appendChild(innerLink);
+    return li;
+  }
 
 
-	function buildItem(request) {
-		var innerLink = document.createElement('a');
-		innerLink.setAttribute('href', request.url);
-		innerLink.appendChild(document.createTextNode(request.label));
-		var li = document.createElement('li');
-		li.appendChild(innerLink);
-		li.setAttribute('id', request.id.toString());
-		return li;
-	}
+  function buildItem(request) {
+    var innerLink = document.createElement('a');
+    innerLink.setAttribute('href', request.url);
+    innerLink.appendChild(document.createTextNode(request.label));
+    innerLink.setAttribute('class', 'down');
+    var li = document.createElement('li');
+    li.appendChild(innerLink);
+    li.setAttribute('id', request.id.toString());
+    return li;
+  }
 
-	function hideNav() {
-		$('navegation').style.display = 'none';
-		$('container').style.marginLeft = '0px';
-		$('button-open').style.display = 'block';
-		$('button-close').style.display = 'none';
-		$('huge-logo').style.display = 'none';
-	}
+  function hideNav() {
+  	hideTranslucent();
+    $('navegation').style.display = 'none';
+    $('container').style.marginLeft = '0px';
+    $('button-open').style.display = 'block';
+    $('button-close').style.display = 'none';
+    $('huge-logo').style.display = 'none';
+  }
 
-	function showNav () {
-		$('navegation').style.display = 'inline-block';
-		$('container').style.marginLeft = '300px';
-		$('button-open').style.display = 'none';
-		$('button-close').style.display = 'block';
-		$('huge-logo').style.display = 'block';
-	}
+  function showNav () {
+    showTranslucent();
+    $('navegation').style.display = 'inline-block';
+    $('container').style.marginLeft = '300px';
+    $('button-open').style.display = 'none';
+    $('button-close').style.display = 'block';
+    $('huge-logo').style.display = 'block';
+  }
 
-	function showActiveSubItems (e) {
-	  var subitems = e.target.parentElement.getElementsByClassName('subitems');
-	  if (subitems.length !== 0 && e.target.nodeName === 'A') {
-		  for (var i = 0; i < subitems.length;  i++) {
-		  	subitems[i].style.display = 'block';
-		  };
-  	  $('translucent').style.display = 'block';
-		}
-	}
+  function showActiveSubItems (e) {
+    var subitems = e.target.parentElement.getElementsByClassName('subitems');
+    if (subitems.length !== 0 && e.target.nodeName === 'A') {
+      if (e.target.className === 'down') {
+        e.target.className = 'up';
+      } else {
+        e.target.className = 'down';
+      }
+      for (var i = 0; i < subitems.length;  i++) {
+        if (subitems[i].style.display === 'block') {
+          subitems[i].style.display = 'none';
+          $('translucent').style.display = 'none';
+        } else {
+          subitems[i].style.display = 'block';
+          $('translucent').style.display = 'block';
+        }
+      }
+    }
+  }
 
-	function hideAllSubItems () {
-		var subitems = $('items').getElementsByClassName('subitems');
-	  for (var i = 0; i < subitems.length;  i++) {
-	  	subitems[i].style.display = 'none';
-	  };
-	}
+  function hideActiveSubItems(e) {
+    var activeSubitem = e.target.parentElement.getElementsByClassName('subitems')[0];
+    var subitems = $('items').getElementsByClassName('subitems');
+    for (var i = 0; i < subitems.length;  i++) {
+      if(subitems[i].style.display === 'block' && subitems[i] !== activeSubitem) {
+        subitems[i].style.display = 'none';
+      }
+    }
+  }
 
-	function attachEvent(o, e, cb) {
-		o.addEventListener(e, cb);
-	}
+  function hideAllSubItems() {
+    var items = $('items');
+    var subitems = $('items').getElementsByClassName('subitems');
+    for (var i = 0; i < subitems.length; i++) {
+      subitems[i].style.display = 'none';
+    }
+    for (var j = 1; j < items.childNodes.length; j++) {
+      items.childNodes[j].firstChild.className = 'down';
+    }
+  }
 
-	function load() {
-		// attach events to toggle views
-		window.$ = $;
-		attachEvent($('button-open'), 'click', showNav);
-		attachEvent($('button-close'), 'click', hideNav);
+
+  function hideTranslucent() {
+    $('translucent').style.display = 'none';
+  }
+
+  function showTranslucent() {
+    $('translucent').style.display = 'block';
+  }
+
+  // Attach events and init app
+  function attachEvent(o, e, cb) {
+    o.addEventListener(e, cb);
+  }
+
+  function load() {
+    attachEvent($('button-open'), 'click', showNav);
+    attachEvent($('button-close'), 'click', hideNav);
+    attachEvent($('items'), 'click', function(e) {
+      if($('translucent').style.display === 'block') {
+        $('translucent').style.display = 'none';
+      }
+      hideActiveSubItems(e);
+      showActiveSubItems(e);
+    });
+    attachEvent($('translucent'), 'click', function() {
+      hideTranslucent();
+      hideAllSubItems();
+      hideNav();
+    });
     init();
-	}
+  }
 
-	attachEvent(window, 'load', load);
-
-	document.getElementById("items").addEventListener("click",function(e) {
-	  if($('translucent').style.display === 'block') {
-  		$('translucent').style.display = 'none';
-   	}
-   	hideAllSubItems();
-   	showActiveSubItems(e);
-  });
-
-
-	document.getElementById("translucent").addEventListener("click",function(e) {
-  	$('translucent').style.display = 'none';
-  	hideAllSubItems();
-	});
-
-	window.$ = $;
+  attachEvent(window, 'load', load);
 
 })();
